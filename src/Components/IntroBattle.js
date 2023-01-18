9a5cac43782e57a6d1e1c0da791d5b4db025dd6b
Hicham Thabti <thabti.hicham@gmail.com>
2023-01-18 15:10:19 +0100

    gym leaders integrated, battle intro page integrated, gym leaders lineups defined

--- src/Components/IntroBattle.js
+++ src/Components/IntroBattle.js
@@ -2,6 +2,7 @@ import { PokeContext } from "../Context/PokeContext";
 import { useContext, useState } from "react";
 import HealthBar from "../assets/health-bar.png";
 import Frame from "../assets/frame.png";
+import {Link} from "react-router-dom"
 
 const IntroBattle = () => {
   const { value4, value5, value1 ,value} = useContext(PokeContext);
@@ -16,13 +17,14 @@ console.log('poke',pokemon[111])
 
 
 
-
   return (
-    <div>
+    <div className="introbattle">
+      <h1 className="introtitle">You team is ready to battle !</h1>
+
       <div className="teamframeintro">
         {playerTeam.length === 4 &&
-          playerTeam.map((e, i) => (
-            <>
+          playerTeam.map((e, i) => 
+            
               <div className="framePoke">
                 <img
                   className="pteamimg"
@@ -32,15 +34,48 @@ console.log('poke',pokemon[111])
                   }
                   alt=""
                 />
-                <div className="textFrame">
-                  {e.name} {e.types[0].type.name}
+                <div className="introtextframe">
+                <p>  Name : {e.name} <br/>Type : {e.types[0].type.name}<br/>Level : 100</p>
                 </div>
-                <img src={HealthBar} style={{ width: "100px" }} alt="" />
               </div>
-            </>
-          ))}
+         
+        )}
       </div>
-    </div>
+      <h1 className='gymleadtitle'>Which Gym Leader do you want to battle ?</h1>
+                  <div className="gymleaders">
+                    <div className="gymleadcolumn">
+                     <Link to="">
+                     <img src="/brock_rock.png" alt="" />
+                     </Link>
+                     <h1>Brock<br/>The Rock/Ground type <br/>Pokemon Master !</h1>
+                     </div>
+
+                     <div className="gymleadcolumn">
+                     <Link to="">
+                     <img src="/misty_water.png" alt="" />
+                     </Link> 
+                     <h1>Misty<br/>The Water type<br/> Pokemon Master !</h1>
+
+                     </div>
+
+                     <div className="gymleadcolumn">
+                     <Link to="">
+                      <img src="/ltsurge_electric.png" alt="" />
+                     </Link> 
+                     <h1>Lt. Surge<br/>The Electric type<br/> Pokemon Master !</h1>
+
+                      </div>
+
+                      <div className="gymleadcolumn">
+                      <Link to="">
+                      <img src="/erika_grass.png" alt="" />
+                      </Link>
+                      <h1>Erika<br/>The Grass type<br/> Pokemon Master !</h1>
+
+                     </div>
+                     </div>
+                     </div>
+
   );
 };
 
