e827d94e876d1d0b044454c23c189f92dfaaaad6
Barbara <barbara@git>
2023-01-18 12:42:47 +0100

    add Notfound and homepage to App

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
-import './App.css';
-import './fonts/pokemon_game.ttf'
-import {Link} from "react-router-dom"
-
+import Homepage from './Components/Homepage';
 
 const App = () => {
 
-
   return (
     <div className="App">
-
-    {/* <img className='homepokelogo' src='/Pokearena_pixel.png' alt="" /> */}
-    <h1 className="hometitle">Battle your way to the Indigo League !</h1>
-    {/* <Link to="/select">
-    <p className='hometext'>Start The Adventure !</p>
-    </Link> */}
- 
+      <Homepage />
     </div>
   );
 }
--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,19 +1,18 @@
 import "../fonts/pokemon_game.ttf";
-import React from "react";
 import "../App.css";
-
 import { Link } from "react-router-dom";
 
 const Homepage = () => {
+
   return (
-    <div className="Homepage">
-      <img className="homepokelogo" src="/Pokearena_pixel.png" alt="" />
+    <div className="App">
+      <img className='homepokelogo' src='/Pokearena_pixel.png' alt="" /> 
       <h1 className="hometitle">Battle your way to the Indigo League !</h1>
       <Link to="/select">
-        <p className="hometext">Start The Adventure !</p>
+        <p className='hometext'>Start The Adventure !</p>
       </Link>
     </div>
   );
 };
 
-export default Homepage;
+export default Homepage
